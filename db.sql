0094399a69d7b2cc7998f21f6d86c4726351fad0
Dengri <grischenkodenis@gmail.com>
2015-10-30 20:41:30 +0200

    adds  field to the table

--- db.sql
+++ db.sql
@@ -1,4 +1,5 @@
 /*AROP DATABASE `shirts4mike`;*/
+DROP DATABASE `sitecontent`;
 CREATE DATABASE `sitecontent` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
 USE `sitecontent`;
@@ -7,7 +8,8 @@ CREATE TABLE IF NOT EXISTS `torrents`(
 `id` int(11) NOT NULL AUTO_INCREMENT,
 `title` varchar(225) DEFAULT NULL,
 `tags` varchar(225) DEFAULT NULL,
-`url` varchar(500) DEFAULT NULL,
+`url` varchar(255) DEFAULT NULL,
+`md5` varchar(50) DEFAULT NULL,
 PRIMARY KEY(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE utf8_unicode_ci;
 
